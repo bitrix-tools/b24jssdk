6ac5f92cb6d025079437c981be2bad9874ff2dc7
Shevchik Igor <s.hevchik.mail@gmail.com>
2026-05-23 22:29:19 +0300

    chore(stage-1): add remaining composables and rest-api examples

---
+++ docs/app/composables/useRestApiVersions.ts
@@ -0,0 +1,28 @@
+export function useRestApiVersions() {
+  const restApiVersion = useCookie(
+    'bitrix24-jssdk-rest-api-version',
+    { default: () => 'rest-api-ver2' }
+  )
+  const { track } = useAnalytics()
+
+  function setRestapiVersion(value: 'rest-api-ver2' | 'rest-api-ver3', source?: string) {
+    restApiVersion.value = value
+    track('RestApiVersion Switched', { restapiVersion: value, source: source || 'search' })
+  }
+
+  const restApiVersions = computed(() => [{
+    label: 'restApi:v2',
+    value: 'rest-api-ver2',
+    onSelect: () => setRestapiVersion('rest-api-ver2')
+  }, {
+    label: 'restApi:v3',
+    value: 'rest-api-ver3',
+    onSelect: () => setRestapiVersion('rest-api-ver3')
+  }].map(f => ({ ...f, active: restApiVersion.value === f.value })))
+
+  return {
+    restApiVersion,
+    setRestapiVersion,
+    restApiVersions
+  }
+}
