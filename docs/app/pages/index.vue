f951fb0a2845f9e2763c602c9cbcec7032d8ccac
Shevchik Igor <s.hevchik.mail@gmail.com>
2026-05-23 22:32:02 +0300

    chore(stage-1): add frame examples, layouts, pages, plugins, utils, workers

---
+++ docs/app/pages/index.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+// import { joinURL } from 'ufo'
+import EncloseTextInCodeTagIcon from '@bitrix24/b24icons-vue/editor/EncloseTextInCodeTagIcon'
+import InfoCircleIcon from '@bitrix24/b24icons-vue/outline/InfoCircleIcon'
+import PlayLIcon from '@bitrix24/b24icons-vue/outline/PlayLIcon'
+import DemonstrationOnIcon from '@bitrix24/b24icons-vue/outline/DemonstrationOnIcon'
+
+const { data: page } = await useAsyncData('index', () => queryCollection('index').first())
+if (!page.value) {
+  throw createError({ status: 404, statusText: 'Page not found', fatal: true })
+}
+
+const config = useRuntimeConfig()
+
+if (import.meta.server) {
+  prerenderRoutes([`${config.public.baseUrl}/raw/index.md`])
+
+  useSchemaOrg([
+    defineSoftwareApp({
+      name: 'Bitrix24 JS SDK',
+      operatingSystem: 'Web',
+      applicationCategory: 'DeveloperApplication',
+      offers: { price: 0, priceCurrency: 'USD' }
+    })
+  ])
+}
+
+useCanonical(`/raw/index.md`)
+
+useSeoMeta({
+  titleTemplate: '%s - Bitrix24 JS SDK',
+  title: page.value.title,
+  description: page.value.description,
+  ogTitle: `${page.value.title} - Bitrix24 JS SDK`,
+  ogDescription: page.value.description
+})
+
+const iconFromIconName = (iconName?: string) => {
+  if (!iconName) {
+    return undefined
+  }
+
+  switch (iconName) {
+    case 'EncloseTextInCodeTagIcon': return EncloseTextInCodeTagIcon
+    case 'InfoCircleIcon': return InfoCircleIcon
+    case 'PlayLIcon': return PlayLIcon
+    case 'DemonstrationOnIcon': return DemonstrationOnIcon
+  }
+
+  return undefined
+}
+</script>
+
+<template>
+  <main v-if="page" class="min-h-[calc(100vh-125px)]">
+    <B24Container class="px-[22px] lg:px-8 py-10 sm:py-16 lg:py-24 relative flex flex-col items-start sm:items-center justify-center gap-[20px]">
+      <h1 class="relative text-label sm:text-center text-5xl sm:text-8xl font-bold mb-0">
+        @bitrix24/b24jssdk <br> Bitrix24 JS SDK
+      </h1>
+
+      <div class="sm:text-center sm:text-4xl mb-2 last:mb-0 text-pretty text-(length:--ui-font-size-xl) leading-(--ui-font-line-height-lg) text-label">
+        {{ page.hero.description }}
+      </div>
+
+      <B24Separator class="my-4" type="dashed" accent="accent" />
+      <div class="mt-2 flex flex-wrap items-start sm:items-center gap-4">
+        <B24Button
+          v-for="link of page.hero.links"
+          :key="link.label"
+          v-bind="link"
+          size="md"
+          :icon="iconFromIconName(link?.iconName)"
+        />
+      </div>
+    </B24Container>
+
+    <B24PageSection :b24ui="{ container: 'py-10 sm:py-10 lg:py-10 gap-0 sm:gap-0' }" />
+  </main>
+</template>
