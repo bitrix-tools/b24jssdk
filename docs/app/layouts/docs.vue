07d3080597c25b331c01a7c30a8f2f18c4c4784e
Shevchik Igor <s.hevchik.mail@gmail.com>
2026-05-23 18:52:53 +0300

    chore(stage-1b): add docs/app remaining examples, layouts, pages, plugins, utils, workers, types

---
+++ docs/app/layouts/docs.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import type { ContentNavigationItem } from '@nuxt/content'
+import { useFilter } from '@bitrix24/b24ui-nuxt/composables'
+
+const navigation = inject<Ref<ContentNavigationItem[]>>('navigation')
+
+const route = useRoute()
+const { scoreItem } = useFilter()
+
+// region Navigation ////
+const { navigationMenuByCategory } = useNavigation(navigation!)
+const filteredNavigation = computed(() => {
+  if (!cleanedSearchTerm.value) {
+    return navigationMenuByCategory.value
+  }
+  return navigationMenuByCategory.value?.filter(item => scoreItem(item, cleanedSearchTerm.value, ['title', 'description']))
+})
+// endregion ////
+
+// region Search ////
+const searchTerm = ref('')
+const isSearchActive = computed(() => route.path.startsWith('/docs/components'))
+const isPanelCollapsed = ref(false)
+const navigationKey = computed(() => `${route.path}-${searchTerm.value ? 'filtered' : 'unfiltered'}`)
+const cleanedSearchTerm = computed(() => {
+  return searchTerm.value
+    .replace(/^B24(?=[A-Z])/, '')
+    .replace(/^b24-/, '')
+})
+
+watch(() => route.path, () => {
+  if (!isSearchActive.value) {
+    searchTerm.value = ''
+    isPanelCollapsed.value = false
+  }
+})
+
+const input = useTemplateRef('input')
+// endregion ////
+
+// region Shortcuts ////
+defineShortcuts({
+  '/': {
+    usingInput: false,
+    handler: () => {
+      input?.value?.inputRef?.focus()
+    }
+  }
+})
+// endregion ////
+</script>
+
+<template>
+  <B24Main>
+    <B24Container class="px-0 sm:px-0">
+      <B24Page class="lg:gap-4">
+        <template #left>
+          <B24PageAside class="scrollbar-thin scrollbar-transparent border-(--ui-color-divider-accent) border-e-1">
+            <div v-if="isSearchActive" class="py-3">
+              <B24Input ref="input" v-model="searchTerm" placeholder="Filter..." class="group">
+                <template #trailing>
+                  <B24Kbd value="/" />
+                </template>
+              </B24Input>
+            </div>
+
+            <B24NavigationMenu
+              :key="navigationKey"
+              :items="filteredNavigation"
+              orientation="vertical"
+              :b24ui="{ linkLeadingBadge: 'mt-auto -top-[4px] left-auto -right-[50px] bg-blue-500' }"
+            />
+          </B24PageAside>
+        </template>
+
+        <slot />
+      </B24Page>
+    </B24Container>
+  </B24Main>
+</template>
